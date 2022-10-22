b6503e64ebe6e8fef447cf23df52c8a0cd5029a1
Richard Angapin <richardangapin@gmx.com>
2022-10-21 20:35:24 -0600

    minor design adjustements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,6 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -11,33 +10,18 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Freediving Nicaragua Website",
+      description: <a href="https://freediving-nicaragua.com/">Check it out</a>,
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Freediving Nicaragua Theme",
+      description: <a href="https://freediving-nicaragua.com/">Check it out</a>,
       imgUrl: projImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Freediving Nicaragua Plugin",
+      description: <a href="https://freediving-nicaragua.com/">Check it out</a>,
       imgUrl: projImg3,
     },
   ];
@@ -92,7 +76,6 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   )
 }
